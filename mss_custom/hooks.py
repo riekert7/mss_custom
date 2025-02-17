c7f5a9428700be3bb829bed4726fe8aeee9b55cb
riekert7 <riekert7@gmail.com>
2025-02-17 08:13:02 +0200

    Attemted export for Mobile Scan dev to live

--- mss_custom/hooks.py
+++ mss_custom/hooks.py
@@ -243,14 +243,17 @@ app_license = "mit"
 # }
 
 fixtures = [
-  {"dt": "Doctype", "filters": [["name", "=", "WhatsApp Dialogue"]]},
+  {"dt": "Custom Field", "filters": []},
+  {"dt": "Server Script", "filters": []},
+  {"dt": "Client Script", "filters": []},
+  {"dt": "DocType", "filters": [["name", "=", "WhatsApp Dialogue"]]},
   {"dt": "WhatsApp Dialogue", "filters": []},  # Includes all records
-  {"dt": "Doctype", "filters": [["name", "=", "WhatsApp Dialogue Item"]]},
-  {"dt": "WhatsApp Dialogue Item", "filters": []},  # Includes all records
-  {"dt": "Doctype", "filters": [["name", "=", "WhatsApp Dialogue Exchange"]]},
+  # {"dt": "DocType", "filters": [["name", "=", "WhatsApp Dialogue Item"]]},
+  # {"dt": "WhatsApp Dialogue Item", "filters": []},  # Includes all records
+  {"dt": "DocType", "filters": [["name", "=", "WhatsApp Dialogue Exchange"]]},
   {"dt": "WhatsApp Dialogue Exchange", "filters": []},  # Includes all records
-  {"dt": "Doctype", "filters": [["name", "=", "WhatsApp Dialogue Exchange Item"]]},
-  {"dt": "WhatsApp Dialogue Exchange Item", "filters": []},  # Includes all records
+  # {"dt": "DocType", "filters": [["name", "=", "WhatsApp Dialogue Exchange Item"]]},
+  # {"dt": "WhatsApp Dialogue Exchange Item", "filters": []},  # Includes all records
   {"dt": "WhatsApp Settings", "filters": []},
   {"dt": "Issue Type", "filters": []},
   {"dt": "Issue Priority", "filters": []}
